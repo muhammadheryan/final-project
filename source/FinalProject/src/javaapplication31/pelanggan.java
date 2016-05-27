33f9b6fc3502384970a1141dcf3733805f5d2106
Muhammad Bayu Lesmana <bayyulesmana22@gmail.com>
2016-05-27 19:19:55 +0700

    membuat kelas dengan method public void dan public string getterdaftar

---
+++ source/FinalProject/src/javaapplication31/pelanggan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication31;
+import java.util.Calendar;
+public class pelanggan {
+    String IDpelanggan,NIK,nama,TTL;
+    java.util.Date trdftr= new java.util.Date();
+    java.text.SimpleDateFormat tampiltrdftr= new java.text.SimpleDateFormat("dd/MM/yyyy");
+    public void terdaftar(int a, int b, int c){
+     trdftr.setDate(a);
+     trdftr.setMonth(b);
+     trdftr.setYear(c);
+    }public String gettrdftr(){
+                return String.format(trdftr.getDate()+"/"+trdftr.getMonth()+"/"+trdftr.getYear());
+    }
+}
