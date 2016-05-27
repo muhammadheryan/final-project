4478a40d7cce349e1c5e3aabee5cb0e964c8d80c
Muhammad Bayu Lesmana <bayyulesmana22@gmail.com>
2016-05-27 19:17:15 +0700

    membuat kelas transaksi dengan method gettggl,setkode, set chekin, get chekin, get chekout

---
+++ source/FinalProject/src/javaapplication31/transaksi.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication31;
+
+public class transaksi extends kamar {
+
+    String kodeTransaksi;
+    java.util.Date skrng = new java.util.Date();
+    java.text.SimpleDateFormat tampil = new java.text.SimpleDateFormat("dd/MM/yyyy");
+    java.util.Date tglcheckin = new java.util.Date();
+    java.util.Date tglcheckout = new java.util.Date();
+    java.text.SimpleDateFormat tampilcheckout = new java.text.SimpleDateFormat("dd/MM/yyyy");
+
+    public String gettgl() {
+        return String.format(tampil.format(skrng));
+    }
+
+    public void setkode(String a) {
+        kodeTransaksi = a;
+    }
+
+    public void setcheckin(int a, int b, int c) {
+        tglcheckin.setDate(a);
+        tglcheckin.setMonth(b);
+        tglcheckin.setYear(c);
+    }
+
+    public String getcheckin() {
+        return String.format(tglcheckin.getDate()+"/"+tglcheckin.getMonth()+"/"+tglcheckin.getYear());
+    }
+
+    public void setcheckout(int a) {
+        tglcheckout.setDate(tglcheckin.getDate() + a);
+    }
+
+    public String getcheckout() {
+        return String.format(tampilcheckout.format(tglcheckout));
+    }
+
+    public void menu() {
+        System.out.println("MENU : ");
+        System.out.println("1.Daftar Kelas dan Harga kamar");
+        System.out.println("2.Daftar Discount kelas kamar");
+        System.out.println("3.Daftar kamar kosong");
+        System.out.println("4.Transaksi pemesanan kamar");
+        System.out.println("5.Cetak transaksi");
+        System.out.println("6.Keluar ");
+    }
+}
